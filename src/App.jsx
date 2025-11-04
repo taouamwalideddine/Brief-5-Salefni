9ec2770600e6fe25f8d3b4a8d1a94d200b442a28
taouamwalideddine <wtaouam@gmail.com>
2025-11-04 17:20:47 +0100

    Added Router logic to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+import SumulationPage from "./pages/SimulationPage";
+import AdminDashboard from "./pages/AdminDashboard";
+import RequestPage from "./pages/RequestPage";
+
+const App = () => {
+    return (
+        <Router>
+            <nav style={{marginBottom : "20px"}}>
+                <link to="/">Simulation</link>|{" "}
+                <link to="/request">Credit Request</link>|{" "}
+                <link to="/admin">Admin Dashboard</link>
+            </nav>
+            <Route>
+                <Route path = "/" element={<SimulationPage />}></Route>
+                <Route path = "/request" element={<RequestPage />}></Route>
+                <Route path = "/admin" element={<AdminDashboard />}></Route>
+            </Route>
+        </Router>
+    );
+};
+
+export default App;
