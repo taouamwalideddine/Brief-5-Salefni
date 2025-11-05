fc6661552eded4fd7fca84c0d14263dbf7dd9145
taouamwalideddine <wtaouam@gmail.com>
2025-11-05 01:59:31 +0100

    Refactor App component and remove unused pages; update import paths to fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-import SumulationPage from "./pages/SimulationPage";
-import AdminDashboard from "./pages/AdminDashboard";
-import RequestPage from "./pages/RequestPage";
+import SimulationPage from "./pages/SimulationPage"; 
 
 const App = () => {
-    return (
-        <Router>
-            <nav style={{marginBottom : "20px"}}>
-                <link to="/">Simulation</link>|{" "}
-                <link to="/request">Credit Request</link>|{" "}
-                <link to="/admin">Admin Dashboard</link>
-            </nav>
-            <Route>
-                <Route path = "/" element={<SimulationPage />}></Route>
-                <Route path = "/request" element={<RequestPage />}></Route>
-                <Route path = "/admin" element={<AdminDashboard />}></Route>
-            </Route>
-        </Router>
-    );
+  return (
+    <Router>
+      <nav style={{ marginBottom: "20px" }}>
+        <Link to="/">Simulation</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<SimulationPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
--- src/pages/SimulationPage.jsx
+++ src/pages/SimulationPage.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-
-const SumulationPage = () => {
-    return (
-        <div>
-            <h1>credit simulation</h1>
-            <p>Form and calculation will go here.</p>
-        </div>
-    )
-}
-export default SumulationPage
+const SimulationPage = () => (
+  <div>
+    <h1>Credit Simulation</h1>
+    
+    <p>Form and calculation will go here.</p>
+  </div>
+);
+export default SimulationPage;
